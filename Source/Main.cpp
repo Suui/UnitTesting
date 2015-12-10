e4c30937bbf1277166f3fd65c276aa97e9849afb
Suui <suui.gd@gmail.com>
2015-12-10 19:35:32 +0100

    Removed unneeded for now

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -36,24 +36,6 @@ std::map<std::string, TestClass> TestRepository::testClasses;
 std::vector<TestFunction> TestRepository::testFunctions;
 
 
-
-class TestRegistrar
-{
-public:
-
-	TestRegistrar(TestClass& testClass, std::string testClassName)
-	{
-		TestRepository::testClasses.insert(std::make_pair(testClassName, testClass));
-	}
-};
-
-#define REGISTER_TEST(ClassName)					\
-TestClass ClassName;								\
-TestRegistrar registrar##ClassName(ClassName, #ClassName);
-
-REGISTER_TEST(TestShould);
-
-
 class TestFunctionRegistrar
 {
 public:
@@ -65,28 +47,14 @@ public:
 };
 
 
-#define TEST_CLASS(ClassName)	\
-class ClassName : public TestClass
-
-#define TEST(TestName)				\
-static void TestName()				\
-
-#define IMPLEMENT()			\
-TestFunctionRegistrar registrar_BooleanShould_be_true_when_true(BooleanShould::be_true_when_true);
-
-#define IMPLEMENTATION(TestName)		\
-TestFunctionRegistrar registrar_BooleanShould_be_false_when_false(BooleanShould::be_false_when_false);	\
-void TestName()
-
 #define NAME_AND_LINE_STRINGIFIED(name, line) name##line
 #define NAME_AND_LINE(name, line) NAME_AND_LINE_STRINGIFIED(name, line)
 #define GET_UNIQUE_NAME() NAME_AND_LINE(FluentUnrealUnit, __LINE__)
 
-//#define REGISTER(TestName)				\
-//TestFunctionRegistrar registrar_BooleanShould_report_error_when_unmatched(TestName);	\
-//void TestName()
+#define TEST_CLASS(ClassName)	\
+class ClassName : public TestClass
 
-#define REGISTER(TestName)				\
+#define TEST(TestName)				\
 TestFunctionRegistrar GET_UNIQUE_NAME()(TestName);	\
 void TestName()
 
@@ -95,28 +63,27 @@ TEST_CLASS(BooleanShould)
 {
 public:
 
-	TEST(report_error_when_unmatched);
+	static void report_error_when_unmatched();
 
-	TEST(be_true_when_true);
+	static void be_true_when_true();
 
-	TEST(be_false_when_false);
+	static void be_false_when_false();
 };
 
 
-REGISTER(BooleanShould::report_error_when_unmatched)
+TEST(BooleanShould::report_error_when_unmatched)
 {
 	std::cout << "Report error when unmatched" << std::endl;
 }
 
 
-IMPLEMENT()
-void BooleanShould::be_true_when_true()
+TEST(BooleanShould::be_true_when_true)
 {
 	std::cout << "Be True When True!" << std::endl;
 }
 
 
-IMPLEMENTATION(BooleanShould::be_false_when_false)
+TEST(BooleanShould::be_false_when_false)
 {
 	std::cout << "Be False When False!" << std::endl;
 }
