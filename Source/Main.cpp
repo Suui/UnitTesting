536da7de0c8e5530bb2ad20ab210aabcfd194f27
Suui <suui.gd@gmail.com>
2016-02-24 20:30:37 +0100

    Added info + testing stuff

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -1,4 +1,8 @@
 ﻿#include <iostream>
+#include <string>
+#include <fstream>
+#include <vector>
+#include <sstream>
 
 
 #define Test()
@@ -24,7 +28,41 @@ public:
 };
 
 
+std::vector<std::string> Split(std::string& string)
+{
+	std::stringstream stream(string);
+	std::string element;
+	auto elements = std::vector<std::string>();
+
+	while (std::getline(stream, element, ' '))
+	{
+		elements.push_back(element);
+	}
+
+	return elements;
+}
+
+typedef void (BooleanShould::*TestFunction)();
+
+#define TEST_FUNCTION_PTR(ClassName, FunctionName) &ClassName::FunctionName
+
 int main()
 {
+	std::ifstream fileStream("D:\\Projects\\Programming\\Cpp\\UnitTesting\\Source\\Main.cpp");
+	std::string line;
 	
+	while (std::getline(fileStream, line))
+	{
+		if (line.find("Test()") != std::string::npos)
+		{
+			if (line.find("#define") != std::string::npos) continue;
+			if (line.find("find") != std::string::npos) continue;
+
+			std::getline(fileStream, line);
+			auto words = Split(line);
+			auto functionName = words[1].substr(0, words[1].length() - 2);
+			std::cout << "Test Function with name: " << functionName << std::endl;
+//			TestFunction function = TEST_FUNCTION_PTR("BooleandShould", functionName);
+		}
+	}
 }
